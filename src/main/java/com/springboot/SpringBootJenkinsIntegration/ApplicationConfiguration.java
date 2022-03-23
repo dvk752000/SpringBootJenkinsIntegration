3e4aab7576b8b922ee1b44849557a6a7b86a0a4e
vadudduk <dvk7520000@gmail.com>
2022-03-23 22:06:23 +0530

    Implementing POJO

---
+++ src/main/java/com/springboot/SpringBootJenkinsIntegration/ApplicationConfiguration.java
@@ -0,0 +1,27 @@
+package com.springboot.SpringBootJenkinsIntegration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class ApplicationConfiguration {
+	
+	private String version;
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	@Override
+	public String toString() {
+		return "\nApplicationConfiguration [version=" + version + "]\n";	
+	}
+	
+	
+
+}
