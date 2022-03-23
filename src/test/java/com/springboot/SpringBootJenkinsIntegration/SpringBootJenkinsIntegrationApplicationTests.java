abfec7f54a5ac588d09c78eab36e35d80814c50b
vadudduk <dvk7520000@gmail.com>
2022-03-23 12:59:43 +0530

    First Commit

---
+++ src/test/java/com/springboot/SpringBootJenkinsIntegration/SpringBootJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.SpringBootJenkinsIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
